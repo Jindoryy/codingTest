3c52449eccb293b69b293613c7214c26f552d0f7
Jindoryy <tyjk8997@gmail.com>
2025-02-10 22:12:56 +0900

    [ Sol ○ ] Programmers - 1845

---
+++ src/programmers/level1/hash/P1845.java
@@ -0,0 +1,34 @@
+package programmers.level1.hash;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+
+        Set<Integer> pickList = new HashSet<>();
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!pickList.contains(nums[i])) {
+                pickList.add(nums[i]);
+            }
+        }
+
+        if (pickList.size() > nums.length / 2) {
+            answer = nums.length / 2;
+        } else {
+            answer = pickList.size();
+        }
+
+        return answer;
+    }
+}
+
+public class P1845 {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(new int[] { 3,3,3,2,2,2}));
+    }
+}
