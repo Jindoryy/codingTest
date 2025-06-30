224d52821845cfa39305a3404a1318a7644a0b6b
Jindoryy <tyjk8997@gmail.com>
2025-06-30 22:00:29 +0900

    [ Sol O ] Programmers - 159993

--- src/programmers/bfs/level2/P159993.java
+++ src/programmers/bfs/level2/P159993.java
@@ -2,69 +2,66 @@ package programmers.bfs.level2;
 
 import java.util.*;
 
-// 레버 찾는 BFS, 출구 찾는 BFS 총 2번 돌리면 되지 않나?
-/*
-    1. 레버 찾는 BFS 돌린 후 큐 초기화 하는거 생각 못함.
-    2. 레버 찾는 BFS (O), 출구 찾는 BFS(X)인 경우 생각 못함.
-*/
 class Solution {
 
     static int n, m;
-    static int sx, sy;
-    static int[] dx = {0, 0, -1, 1}, dy = {-1, 1, 0, 0};
-    static boolean[][] visited;
+    static int sx, sy, ex, ey;
     static char[][] board;
-    static Queue<int[]> queue = new LinkedList<>();
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1}, dy = {1, -1, 0, 0};
+    static int answer;
 
     public int solution(String[] maps) {
 
         n = maps.length;
         m = maps[0].length();
-        board = new char[n][m];
 
+        board = new char[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
+
             board[i] = maps[i].toCharArray();
         }
 
+        // 시작 위치 좌표 저장
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+
                 if (board[i][j] == 'S') {
                     sx = i;
                     sy = j;
-                    break;
                 }
             }
         }
 
-        // 총 이동횟수
-        int cnt = 0;
-
         // 레버 찾는 BFS
-        visited = new boolean[n][m];
         int check = bfs(sx, sy, 'L');
         if (check == 0) return -1;
-        cnt += check;
+        answer += check;
 
-        // 도착지 찾는 BFS
-        queue.clear();
+        // 방문기록 초기화
         visited = new boolean[n][m];
+
+        // 출구 찾는 BFS
         check = bfs(sx, sy, 'E');
         if (check == 0) return -1;
-        cnt += check;
+        answer += check;
 
-        return cnt;
+        return answer;
     }
 
-    public int bfs(int x, int y, char target) {
+    private int bfs(int x, int y, char target) {
 
-        queue.add(new int[]{x, y, 0});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y, 0});
         visited[x][y] = true;
 
         while (!queue.isEmpty()) {
-            int[] p = queue.poll();
-            int px = p[0];
-            int py = p[1];
-            int cnt = p[2];
+
+            int[] qPoll = queue.poll();
+            int px = qPoll[0];
+            int py = qPoll[1];
+            int cnt = qPoll[2];
 
             if (board[px][py] == target) {
                 sx = px;
@@ -73,15 +70,17 @@ class Solution {
             }
 
             for (int k = 0; k < 4; k++) {
+
                 int mx = px + dx[k];
                 int my = py + dy[k];
 
                 if (!isRange(mx, my)) continue;
                 if (visited[mx][my]) continue;
 
+                // 모든 통로, 출구, 레버, 시작점은 여러 번 지나갈 수 있음
                 if (board[mx][my] != 'X') {
+                    queue.add(new int[] {mx, my, cnt+1});
                     visited[mx][my] = true;
-                    queue.add(new int[]{mx, my, cnt+1});
                 }
             }
         }
@@ -89,7 +88,8 @@ class Solution {
         return 0;
     }
 
-    public boolean isRange(int mx, int my) {
+    private boolean isRange(int mx, int my) {
+
         return 0 <= mx && mx < n && 0 <= my && my < m;
     }
 }
