dc6f293ac02b3ee9f57afc00643186ccad2aebd8
Jindoryy <tyjk8997@gmail.com>
2025-07-02 23:27:37 +0900

    [ Sol O ] Programmers - 43163

--- src/programmers/bfs/level3/P43163.java
+++ src/programmers/bfs/level3/P43163.java
@@ -5,6 +5,7 @@ import java.util.*;
 class Solution {
 
     static class Transfer {
+
         String word;
         int cnt;
 
@@ -17,46 +18,41 @@ class Solution {
     static int n;
     static boolean[] visited;
     static Queue<Transfer> q = new LinkedList<>();
-    static int answer;
 
     public int solution(String begin, String target, String[] words) {
 
-        answer = Integer.MAX_VALUE;
         n = words.length;
-        visited = new boolean[n];
-
         q.add(new Transfer(begin, 0));
+
+        visited = new boolean[n];
         while (!q.isEmpty()) {
+
             Transfer t = q.poll();
             String word = t.word;
             int cnt = t.cnt;
 
             if (word.equals(target)) {
-                answer = Math.min(answer, cnt);
+                return cnt;
             }
 
             for (int i = 0; i < n; i++) {
-                int diffCnt = 0;
-                if (!visited[i]) {
-                    if (diff(word, words[i]) == 1) {
-                        visited[i] = true;
-                        q.add(new Transfer(words[i], cnt+1));
-                    }
+
+                if (!visited[i] && diff(word, words[i]) == 1) {
+                    visited[i] = true;
+                    q.add(new Transfer(words[i], cnt+1));
                 }
             }
         }
 
-        if (answer == Integer.MAX_VALUE) {
-            return 0;
-        }
-        return answer;
+        return 0;
     }
 
-    public int diff(String a, String b) {
-        int diffCnt = 0;
+    private int diff(String a, String b) {
 
+        int diffCnt = 0;
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) {
+
                 diffCnt += 1;
             }
         }
